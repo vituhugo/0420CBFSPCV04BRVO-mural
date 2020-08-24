8107a8aaf15fdbb45edb139f74ffa40b91f41e88
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-24 15:32:35 -0300

    pagina de mural

---
+++ resources/views/mural.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meu mural</title>
+</head>
+<body>
+
+    <ul class="list">
+        <li>
+            <h1>Anônimo</h1>
+            <p>
+                Algum comentário do usuário anonimo
+            </p>
+        </li>
+        <li>
+            <h1>Anônimo</h1>
+            <p>
+                Algum outro comentário do anônimo
+            </p>
+        </li>
+        <li>
+            <h1>Anônimo</h1>
+            <p>
+                Algum outro comentário mais antigo ainda do anônimo
+            </p>
+        </li>
+    </ul>
+</body>
+</html>
