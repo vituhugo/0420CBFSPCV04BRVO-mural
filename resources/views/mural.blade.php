05174dc0b2caad453d36f7cb1b5522e534334e92
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-24 15:51:26 -0300

    melhorias mural

--- resources/views/mural.blade.php
+++ resources/views/mural.blade.php
@@ -4,29 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Meu mural</title>
 </head>
 <body>
-
-    <ul class="list">
-        <li>
-            <h1>Anônimo</h1>
-            <p>
-                Algum comentário do usuário anonimo
-            </p>
-        </li>
-        <li>
-            <h1>Anônimo</h1>
-            <p>
-                Algum outro comentário do anônimo
-            </p>
-        </li>
-        <li>
-            <h1>Anônimo</h1>
-            <p>
-                Algum outro comentário mais antigo ainda do anônimo
-            </p>
-        </li>
-    </ul>
+    <div class="container">
+        <h1 class="title mt-4">Mural</h1>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <h2>Anônimo</h2>
+                <p>
+                    Algum comentário do usuário anonimo
+                </p>
+            </li>
+            <li class="list-group-item">
+                <h2>Anônimo</h2>
+                <p>
+                    Algum outro comentário do anônimo
+                </p>
+            </li>
+            <li class="list-group-item">
+                <h2>Anônimo</h2>
+                <p>
+                    Algum outro comentário mais antigo ainda do anônimo
+                </p>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
