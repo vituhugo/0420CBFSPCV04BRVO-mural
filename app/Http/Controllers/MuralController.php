7e956a7662ba1d732981401348e293525b747908
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-09-02 17:19:56 -0300

    api

--- app/Http/Controllers/MuralController.php
+++ app/Http/Controllers/MuralController.php
@@ -11,4 +11,34 @@ class MuralController extends Controller
         $posts = Post::all();
         return view('mural', ['posts' => $posts]);
     }
+
+    public function criarPost(Request $request) {
+        //Salva um post no banco.
+        $post = new Post();
+        $post->texto = $request->texto;
+        $post->user = $request->user;
+        $post->save();
+
+        return response()->json($post);
+    }
+
+    public function editarPost(Request $request, $id) {
+        $post = Post::find($id);
+        $post->texto = $request->texto;
+        $post->user = $request->user;
+        $post->save();
+
+        return response()->json($post);
+    }
+
+    public function deletar(Request $request, Post $post) {
+        $post->delete();
+
+        return response()->json(["mensagem" => "registro deletado."]);
+    }
+
+    public function listar(Request $request) {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// /api/post
+Route::post('post', 'MuralController@criarPost');
+Route::put('post/{id}', 'MuralController@editarPost');
+Route::delete('post/{post}', 'MuralController@deletar');
+Route::get('post', 'MuralController@listar');
